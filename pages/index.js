2ef338d7b14bf705082ae2be0773177454526210
yjcho <jinjanic91@gmail.com>
2021-04-18 17:50:02 +0900

    first init pj

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,56 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from 'react';
 
-export default function Home() {
+// parameter 신경 안씀
+const ArrowFunction = _ => <div><h2>I am arrow</h2></div>
+
+function CompA() {
+  //JSX - Javascript XML
+  // return (
+  //   <>
+  //     <h1>CompA</h1>
+  //     <p>CompA paragraph</p>
+  //     <CompB />
+  //   </>
+  // )
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    // without JSX
+    React.createElement(React.Fragment, null, 
+    React.createElement("h1", null, "comp a"),
+    React.createElement("p", null, "Hello comp a"),
+    React.createElement(CompB)
+    )
   )
 }
+function CompB() {
+  return (
+    <>
+      <h1>CompB</h1>
+      <p>Hello compB</p>
+    </>
+  )
+}
+
+//JSX - Javascript XML
+class CompC extends React.Component {
+  render() {
+    return (
+     <>
+      <h1>hello compc</h1>
+     </>
+    );
+  }
+}
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>hello world</h1>
+      <CompA />
+      <ArrowFunction />
+    </>
+  )
+}
+
+
+export default HomePage
