2e16ead9d34b086e0f5d7e0eb6bff2d14d2670dc
yjcho <jinjanic91@gmail.com>
2021-04-19 23:12:34 +0900

    state props

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
 // parameter 신경 안씀
 const ArrowFunction = _ => <div><h2>I am arrow</h2></div>
@@ -22,11 +23,16 @@ function CompA() {
     )
   )
 }
-function CompB() {
+const CompB = ({ myProp }) => {
+  
+  useEffect(() => {
+    console.log('prop has changed!');
+  }, [myProp]);
+  
   return (
     <>
       <h1>CompB</h1>
-      <p>Hello compB</p>
+      <p>Hello compB myValue: {myProp} </p>
     </>
   )
 }
@@ -42,12 +48,59 @@ class CompC extends React.Component {
   }
 }
 
+const BtnSection = styled.div`
+  margin-top: 10px;
+`;
+
+const MyBtn = styled.button`
+  width: 150px;
+  height: 48px;
+  padding: 20px;
+  background: #fff;
+  border: 1px solid blue;
+  border-radius: 4px;
+  outline: none;
+`;
+
 const HomePage = () => {
+  // const value = useState(0);
+  const [myValue, setMyValue] = useState(10);
+
+  // const incrementVal = () => {
+  //   setMyValue(myValue + 1);
+  // }
+
+  // const decrementVal = () => {
+  //   setMyValue(myValue - 1);
+  // }
+
+  useEffect(() => {
+    console.log('has changed!');
+  }, [myValue]);
+
+  const handleChange = (increment) => {
+    setMyValue(myValue + increment);
+  }
+
   return (
     <>
-      <h1>hello world</h1>
-      <CompA />
-      <ArrowFunction />
+      <h1>CurrentValue: {myValue}</h1>
+      <BtnSection>
+      
+      {/* 각각 다른 함수 */}
+      {/* <MyBtn onClick={incrementVal}>+</MyBtn>
+      <MyBtn onClick={decrementVal}>-</MyBtn> */}
+      
+      {/* 하나의 함수로 컨트롤 */}
+      {/* <MyBtn onClick={() => handleChange(+1)}>+</MyBtn>
+      <MyBtn onClick={() => handleChange(-1)}>-</MyBtn> */}
+
+      {/* 함수 없이 컨트롤 */}
+      <MyBtn onClick={() => setMyValue(myValue + 1)}>+</MyBtn>
+      <MyBtn onClick={() => setMyValue(myValue - 1)}>-</MyBtn>
+      </BtnSection>
+      <CompB myProp={myValue} />
+      {/* <ArrowFunction /> */}
     </>
   )
 }
